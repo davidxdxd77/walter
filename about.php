bb1ef26bf0071bc724046e0fa84248091cbcd6a7
davidxdxd77@gmail.com <abcde12345>
2015-07-16 12:30:25 -0400

    agregando about

--- about.php
+++ about.php
@@ -88,6 +88,12 @@
                     <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
                 </div>
             </div>
+            <br>
+            <br>
+            <br>
+                <p>
+                    <a class="btn btn-default btn-lg" href="index.php"> &laquo; Volver</a>
+                </p>
             <!-- /.row -->
         </div>
         <hr>
